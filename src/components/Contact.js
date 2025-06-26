1f5e14882139bae19b01b8bc49c61bdb675f7c51
Hashir39 <126954940+Hashir39@users.noreply.github.com>
2025-06-26 15:07:19 +0500

    Update Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,7 +5,7 @@ function Contact({src,name }) {
     <div className='flex items-center space-x-3 mb-2 relative hover:bg-gray-200 cursor-pointer p-2 rounded-xl'>
         <img src={src} height={50} width={50} style={{objectFit:'cover'}} alt="" />
         <p>{name}</p> 
-        <p className='absolute bottom-2 left-6 bg-green-400 h-3 w-3 rounded-full'></p>
+        <p className='absolute bottom-2 left-6 bg-green-400 h-4 w-3 rounded-full'></p>
     </div>
   )
 }
